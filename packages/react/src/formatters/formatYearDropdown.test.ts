92f1d49abd8df849b345006cc5d3de0541107994
cjkihl <carljohan.kihl@gmail.com>
2025-07-15 14:02:23 +0800

    remove react

--- packages/react/src/formatters/formatYearDropdown.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test";
-import { defaultDateLib } from "../utils/testdatelib";
-import { formatYearDropdown } from "./formatYearDropdown";
-
-test("should return the formatted weekday name", () => {
-	expect(formatYearDropdown(new Date(2022, 0), defaultDateLib)).toEqual("2022");
-});
--- packages/react/src/utils/testdatelib.ts
+++
@@ -1,7 +0,0 @@
-import { DateLib } from "../classes";
-
-export const defaultDateLib = new DateLib({
-	locale: "en-US",
-	numerals: "latn",
-	timeZone: "UTC",
-});
